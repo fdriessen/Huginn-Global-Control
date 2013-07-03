dbb0f1223e1c2bf07294dd6997c02ef2ff950757
stevenvandervlugt <steven.vandervlugt@gmail.com>
2013-07-03 21:50:12 +0200

    Restored a bug, instead of 50 lines only 1 line was copied to the temp memory which is used to send the depth data. Added obstacle detection, obstacles are detected in the center region of the Kinect between 50 and 100cm (approx) distance. Command line argument 1 specifies the frame rate, if a (random) second command line argument is specified the obstacle detection is turned on. Obstacle detection is now provided as printf and by changing the led color of the Kinect. This will have to be changed to a pipe to communicate to the global control. Normally the Kinect led is blinking green, when obstacle detection is on the led is continuous green. When an object is detected in one frame the led is continous yellow, when an object is detected in more than 3 out of 10 frames (moving average filter) the led is continuous red.

--- kinect/kinect_stream/main.cpp
+++ kinect/kinect_stream/main.cpp
@@ -78,6 +78,22 @@ int main(int argc, char **argv)
 	int		key;
 	bool use_depth_image;
 	int i,j;
+	bool flag_obstacle_detection_on = FALSE;
+	int usleep_time_main = 0;
+	int obstacle_MAF; //moving average filter
+	int obstacle_MAF_counter = 0;
+	bool flag_obstacle_MAF = FALSE;
+	freenect_led_options led = (freenect_led_options)LED_BLINK_GREEN;
+	
+	/*
+		LED_OFF              = 0, //< Turn LED off
+		LED_GREEN            = 1, //< Turn LED to Green
+		LED_RED              = 2, //< Turn LED to Red
+		LED_YELLOW           = 3, //< Turn LED to Yellow
+		LED_BLINK_GREEN      = 4, //< Make LED blink Green
+		// 5 is same as 4, LED blink Green
+		LED_BLINK_RED_YELLOW = 6, //< Make LED blink Red/Yellow
+	*/
 	
 	// pointer to the freenect context
  	freenect_context *ctx;
@@ -97,9 +113,15 @@ int main(int argc, char **argv)
 	if(!argv[1]) quit("specify frame rate", 1);
 	//if(((float)atoi(argv[1])<1)||((float)atoi(argv[1])>9)) quit("frame rate should be an int 1 ... 10", 1);
 	int framerate = (int)atoi(argv[1]);
-	int usleep_time_main = (1000000/framerate);
-	usleep_time_udp = (1000000/framerate)/3;
+	if(argv[2])
+	{
+		flag_obstacle_detection_on = TRUE;
+		usleep_time_main = (1000000/framerate)-110000; //emperically dertermined less time out
+		obstacle_MAF = 0;	
+	}
+	else usleep_time_main = (1000000/framerate)-10000; //emperically dertermined less time out
 	
+	usleep_time_udp = usleep_time_main/3;
 	
 	// Set the tilt angle (in degrees)
   	freenect_sync_set_tilt_degs(0, 0); //(level, device_id)
@@ -132,11 +154,17 @@ int main(int argc, char **argv)
 	
 	while(1) {
 
-			unsigned int timestamp;
-			static char *data = 0;
+		unsigned int timestamp;
+		static char *data = 0;
+		IplImage *depth = 0;
+		int obstacle_counter[6];
+		
+		fprintf(stdout, "fps : %.2f\n", calculeFrameRate());
+		
+		if (!depth) depth = cvCreateImageHeader(cvSize(DEPTH_HEIGHT,DEPTH_WIDTH), 16, 1);
 			
-			if (freenect_sync_get_depth((void**)&data, &timestamp, 0, FREENECT_DEPTH_11BIT))
-					quit("kinect() get depth failed", 1);
+		if (freenect_sync_get_depth((void**)&data, &timestamp, 0, FREENECT_DEPTH_11BIT))
+			quit("kinect() get depth failed", 1);
 					
 			pthread_mutex_lock(&mutex);
 				memcpy(depth_data, data, DEPTH_WIDTH*DEPTH_HEIGHT*2);			
@@ -144,15 +172,83 @@ int main(int argc, char **argv)
 				is_data_ready = 1; // start sending data over UDP (handled by different thread)
 			pthread_mutex_unlock(&mutex);
 		
+		if(flag_obstacle_detection_on)
+		{
+			cvSetData(depth, data, DEPTH_HEIGHT*2); //copy kinect data into IplImage format, not necesarry but easier
+			// optimalisatie mogelijkheid: met raw kinect data werken ipv deze omweg
+			
+			obstacle_counter[0] = 0; obstacle_counter[1] = 0; obstacle_counter[2] = 0;
+			
+			led = (freenect_led_options)LED_GREEN; //standard green, might be overwritten later
+			flag_obstacle_MAF = FALSE;
+			
+			//for (i = 0; i < DEPTH_HEIGHT*DEPTH_WIDTH; i++) {
+			for (i = (DEPTH_HEIGHT/4); i < (3*(DEPTH_HEIGHT/4)); i++) 
+			{
+				//int lb = ((short *)depth->imageData)[i] % 256;
+				//int ub = ((short *)depth->imageData)[i] / 256; //only check upper byte
+				
+				//if((i>((DEPTH_HEIGHT/4)*DEPTH_WIDTH))&&(i<((DEPTH_HEIGHT/4)*3*DEPTH_WIDTH)))
+				for (j = (DEPTH_WIDTH/4); j < (3*(DEPTH_WIDTH/4)); j++)
+				{
+					// eerste en laatste DEPTH_HEIGHT/4 punten over slaan
+					// eerste en laatste DEPTH_WIDTH/4 punten over slaan
+					int ub = ((short *)depth->imageData)[i*j] / 256; //only check upper byte
+					
+					if(ub<=5)
+					{
+						// optimalisatie: 1 op 4 oid overslaan
+						obstacle_counter[ub]++;
+						if((obstacle_counter[0]>0)||(obstacle_counter[1]>100)||(obstacle_counter[2]>2000))
+						{	
+							if(obstacle_MAF>9)obstacle_MAF = 9;
+							else obstacle_MAF+=2;
+							led = (freenect_led_options)LED_YELLOW;
+							flag_obstacle_MAF = TRUE;
+							break;
+						}
+						
+					}
+				}
+				if(flag_obstacle_MAF) break;
+			}
+			if(!flag_obstacle_MAF)
+			{
+				if(obstacle_MAF<=0) obstacle_MAF = 0;
+				else obstacle_MAF--;
+			}
+			
+			if(obstacle_MAF>3)
+			{
+				printf("### OBSTACLE DETECTED in more than 3 out of 10 frames ###\n");
+				printf("\t0\t1\t2\n");
+				printf("\t%d\t%d\t%d\n",obstacle_counter[0],obstacle_counter[1],obstacle_counter[2]);
+				printf("Obstacle MAF = %d\n",obstacle_MAF);
+				printf("\n");
+				if((obstacle_counter[0]>0)||(obstacle_counter[1]>100)||(obstacle_counter[2]>2000))
+				{
+					led = (freenect_led_options)LED_RED;
+				}
+				else led = (freenect_led_options)LED_YELLOW;
+			}
+			
+			printf("\n");
+		}
+		
+		
 		//usleep(1000); //1 ms
 		//usleep(30000); //30 ms -> 33 fps
 		//usleep(200000); //200ms -> 5fps
 		//usleep(190000); //190ms -> 5fps
 		//usleep(300000); //300ms -> 3fps
 		//usleep(1000000);
+		
+		// Set the LEDs
+		freenect_sync_set_led(led, 0);
+		
 		usleep(usleep_time_main);
 		CLS();		
-		fprintf(stdout, "fps : %.2f\n", calculeFrameRate());
+		
 
 	}
 
@@ -244,7 +340,7 @@ void* streamServer(void* arg)
 				//	do this for every line of DEPTH_HEIGHT
 				*/
 				
-				memcpy(&temp_data[2], &depth_data[j*DEPTH_WIDTH*2], DEPTH_WIDTH*2); // one line to a temp memory
+				memcpy(&temp_data[2], &depth_data[j*DEPTH_WIDTH*2], DEPTH_WIDTH*2*50); // one line to a temp memory
 				memcpy(&temp_data[0], &i, 2); // add sequence number
 				
 				if (sendto(sock, &temp_data[0], sendStringLen, 0, (struct sockaddr *)&broadcastAddr, sizeof(broadcastAddr)) != sendStringLen){
@@ -256,12 +352,12 @@ void* streamServer(void* arg)
 			
 			// last 30 lines
 			
-				memcpy(&temp_data[2], &depth_data[450*DEPTH_WIDTH*2], DEPTH_WIDTH*2); // one line to a temp memory
-				memcpy(&temp_data[0], &i, 2); // add sequence number
+			memcpy(&temp_data[2], &depth_data[450*DEPTH_WIDTH*2], DEPTH_WIDTH*2*30); // one line to a temp memory
+			memcpy(&temp_data[0], &i, 2); // add sequence number
 				
-				if (sendto(sock, &temp_data[0], sendStringLenLast, 0, (struct sockaddr *)&broadcastAddr, sizeof(broadcastAddr)) != sendStringLenLast){
-					quit("socket() sendto error", 1);
-				}
+			if (sendto(sock, &temp_data[0], sendStringLenLast, 0, (struct sockaddr *)&broadcastAddr, sizeof(broadcastAddr)) != sendStringLenLast){
+				quit("socket() sendto error", 1);
+			}
 			
 			if (sendto(sock, EndOfFrame, EndOfFrameStringLen, 0, (struct sockaddr *)&broadcastAddr, sizeof(broadcastAddr)) != EndOfFrameStringLen){
 				quit("socket() sendto error", 1);
